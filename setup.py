d410b6749d4772e49bd1f3f1a789c5a4d16613b4
Uri Laserson <uri.laserson@gmail.com>
2016-12-20 11:45:04 -0500

    Added install boilerplate

---
+++ setup.py
@@ -0,0 +1,33 @@
+# Copyright 2016 Uri Laserson
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from setuptools import setup, find_packages
+
+
+def readme():
+    with open('README.md', 'r') as ip:
+        return ip.read()
+
+
+setup(
+    name='llutil',
+    version='0.1.0',
+    description='Laserson lab utilities',
+    long_description=readme(),
+    author='Uri Laserson',
+    author_email='uri@lasersonlab.org',
+    url='https://github.com/lasersonlab/llutil',
+    packages=find_packages(),
+    license='Apache License, Version 2.0',
+    classifiers=['Programming Language :: Python :: 3'])
